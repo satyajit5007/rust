0d7468187c1593a1ea4dc44641f77a8bdece2da8
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-21 16:31:47 +0530

    test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
-    println!("Hello, world!");
+    let a: i64 = 10;  // Changed to i64 for larger range
+    let b: i64 = 20;
+    println!("addition {}", a + b);
+    println!("subtraction {}", a - b);
+    println!("multiplication {}", a * b);
+    println!("division {}", a / b);
+    println!("modulus {}", a % b);
+
+    println!("these a A number {}", a);
+    println!("these b A number {}", b);
+
+    println!("this is a string");
+
 }
