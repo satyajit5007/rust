224cdd6d0080274fbf43f4205ad28832bb5a2d26
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-21 23:26:57 +0530

    new code

--- src/main2.rs
+++ src/main2.rs
@@ -1,8 +1,12 @@
 fn main() {
     println!("Hello, world!");
-    println!("I'm a Rustacean!");
-    
-    let a: i64 = 4000;
+    println!("removing the old and adding the new");
+    // start new coding => on the owneership and the memory management
+    // ownership is a unique property of Rust
 
-    print!("{}", a);
+    // ownership rules
+    // 1. Each value in Rust has a variable that owns it
+    // 2. There can only be one owner at a time
+    // 3. When the owner goes out of scope, the value will be dropped
+    
 }
