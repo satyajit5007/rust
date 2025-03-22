70ba645c2814cd49e75a9fe827d60a7f4431e60f
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-23 00:49:10 +0530

    rules

--- src/main2.rs
+++ src/main2.rs
@@ -66,10 +66,33 @@
 //     str.push_str(" world");
 // }
 
-fn main(){
-    let mut s1: String = String::from("hello");
-    let s2: &mut String = &mut s1;
-    println!("{}", s2);
+// fn main(){
+//     let mut s1: String = String::from("hello");
+//     let s2: &mut String = &mut s1;
+//     println!("{}", s2);
+//     println!("{}", s1);
+
+// }
+
+// fn main() {
+//     let my_string = String::from("Hello, Rust!");
+//     takes_ownership(&my_string);  // Pass a reference to my_string
+//     println!("{}", my_string);    // This is valid because ownership was not transferred
+// }
+
+// fn takes_ownership(some_string: &String) {
+//     println!("{}", some_string);  // some_string is borrowed and not moved
+// }
+
+
+fn main() {
+    let mut s1 = String::from("Hello");
+    let s2 = &mut s1;
+    update_word(&mut s1);
     println!("{}", s1);
-    
+    println!("{}", s2);
+}
+
+fn update_word(word: &mut String) {
+    word.push_str(" World");
 }
