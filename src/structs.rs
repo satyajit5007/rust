5872d9512914b240fa0eedccd852010b07aa101b
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-24 14:33:02 +0530

    structs

---
+++ src/structs.rs
@@ -0,0 +1,19 @@
+struct User {
+    first_name: String,
+    last_name: String,
+    age: i32,
+}
+
+
+fn main(){
+    let user = User{
+        first_name:String::from("Rohan"),
+        last_name:String::from("Bhosale"),
+        age:26,
+    };
+    println!("UserInfo = {} {} {}",user.first_name, user.last_name, user.age);
+    println!("Hello, world!");
+    println!("{}",user.first_name);         
+    println!("{}",user.last_name);
+    println!("{}",user.age);
+}
